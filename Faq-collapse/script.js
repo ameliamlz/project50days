c92d72d81f230fe95a941580a36b22c8100629e4
ameliaMLZ <2459849298@qq.com>
2022-01-06 16:57:58 +0800

    complete the day11 event-keycodes and day12 faq-collapse

---
+++ Faq-collapse/script.js
@@ -0,0 +1,8 @@
+// 又一次设计toggle的用法
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
