a9b460105be2983bb790db8fd15302033633453f
ameliaMLZ <2459849298@qq.com>
2022-05-03 16:11:49 +0800

    test to change antd theme and failed

---
+++ test-theme/src/App.js
@@ -0,0 +1,15 @@
+// 样式失败了 
+
+import logo from './logo.svg';
+// import './App.css';
+import './App.less';
+
+import {Button} from 'antd'
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
